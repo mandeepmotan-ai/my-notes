c8513808544d0788206f8fe11a45343e5b5fa3e4
Mandeep Motan <227738561+mandeepmotan-ai@users.noreply.github.com>
2025-09-05 21:52:10 +0530

    decimal to binary

--- python-for-DSA&Data-Science/python-dsa-practice/03.mathematics-practice/3.05.decimal-to-binary.py
+++ python-for-DSA&Data-Science/python-dsa-practice/03.mathematics-practice/3.05.decimal-to-binary.py
@@ -21,4 +21,40 @@ print("Program: Decimal to Binary Converter")
 print()
 
 def decToBinary(n):
-    
+    binLt = []
+    z = n
+    isNeg = False
+    if str(n)[0] == "-":
+        z = int(-n)
+        isNeg = True
+    while True:
+        binLt.append(z%2)
+        z = z//2
+        if z ==0:
+            break
+    res = "".join(str(x) for x in binLt)
+    if isNeg:
+        return -(int(res))
+    return res
+
+progRun = True
+
+while progRun:
+    try:
+        inNum = int(input("Enter a single integer (-10^9 <= n <= 10^9), n: "))
+        print("Binary version: ",decToBinary(inNum))
+        print()
+        goOn = input("Enter y to continue or n to exit: ")
+        while goOn not in ["y","n"]:
+            print("Choose valid options!!")
+            print()
+            goOn = input("Enter y to continue or n to exit:")
+        print()
+        if goOn == "y":
+           continue
+        elif goOn == "n":
+           progRun = False
+           print("Program terminated successfully!!")
+    except:
+        print("Invalid Input!!")
+        print()
